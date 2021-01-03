5fd4495468ba89e3073a660bf9209846cbe4d6d6
Frank <frank.f.kruse@gmail.com>
2021-01-03 18:20:49 +0100

    Wireframe von Clemens

---
+++ Gesetz.Wireframe/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Gesetz.Wireframe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
